83b83d49e3abebc09a754004b7d86944365af530
Daniel Dabrowski <d.dabrowski@emetriq.com>
2019-04-25 09:55:42 +0200

    feat(validation): check if bucket exists before grepping

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,38 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
+	"github.com/dabdada/s3-grep/config"
+	"github.com/dabdada/s3-grep/s3"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "s3-grep",
+	Use:   "s3-grep [search query]",
 	Short: "Grep contents of an object in S3",
 	Long: "Grep contents of an object in S3",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("s3-grep requires a search query argument")
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		if version {
 			PrintVersion()
 			return
 		}
+		if ok := s3.IsBucket(); !ok {
+			fmt.Printf("The bucket name `%s` was not found in profile `%s`\n", bucketName, profile)
+			return
+		} else {
+			// run the actual grep command
+			return
+		}
+
 		cmd.Usage()
 	},
 }
@@ -28,7 +45,16 @@ func Execute() {
 }
 
 var version bool
+var profile string
+var bucketName string
+var path string
 
 func init() {
-	rootCmd.Flags().BoolVarP(&version, "version", "v", false, "print the version of s3-grep")
+	rootCmd.Flags().BoolVarP(&version, "version", "v", false, "Print the version of s3-grep")
+	rootCmd.Flags().StringVarP(&profile, "profile", "", "", "The AWS profile the S3 bucketName is hosted in")
+	rootCmd.Flags().StringVarP(&bucketName, "bucket", "b", "", "The bucketName name to grep in")
+	rootCmd.Flags().StringVarP(&bucketName, "path", "p", "/", "The path to grep in")
+
+	rootCmd.MarkFlagRequired("profile")
+	rootCmd.MarkFlagRequired("bucket")
 }
---
+++ s3/validators.go
@@ -0,0 +1,20 @@
+package s3
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/dabdada/s3-grep/config"
+)
+
+func IsBucket(session config.AWSSession, bucketName string) bool {
+	svc := s3.New(session.Session)
+	headInput := &s3.HeadBucketInput{
+		Bucket: aws.String(bucketName),
+	}
+	_, err := svc.HeadBucket(headInput)
+
+	if err != nil {
+		return true
+	}
+	return false
+}
